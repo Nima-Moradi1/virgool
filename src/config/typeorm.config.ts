ea377ac3711852e45f6db958b67451d357af3d44
nima moradi <mnima8100@gmail.com>
2025-10-23 10:10:34 +0330

    installed some necessary validator packages , ignored dumb prettier warns , added the swagger BASE config and added to the main file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,3 +1,19 @@
-export function TypeOrmConfig() : TypeOrmModuleOptions {
-    
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export function TypeOrmConfig(): TypeOrmModuleOptions {
+  const { DB_HOST, DB_NAME, DB_PASSWORD, PORT, DB_USERNAME } = process.env;
+  return {
+    type: 'postgres',
+    host: DB_HOST,
+    port: +PORT!,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    autoLoadEntities: false,
+    synchronize: false,
+    entities: [
+      'dist/**/**/**/*.entity{.ts,.js}',
+      'dist/**/**/*.entity{.ts,.js}',
+    ],
+  };
 }
